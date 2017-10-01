c0422bd0831c5806082cdb3d8cb6a186dca8a118
idrummer83 <vodaadminwater@gmail.com>
2017-10-02 00:23:07 +0300

    json

--- phonebook.py
+++ phonebook.py
@@ -1,3 +1,28 @@
+import configparser, json, ast
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+name_file = config['FILE']['filename']
+format_file = config['FILE']['format']
+
+file = "{}.{}".format(name_file,format_file)
+
+def write_to_file(t):
+    with open("{}.{}".format(name_file,format_file), "w") as file:
+        json.dump("{}".format(t), file)
+
+def read_file():
+    with open(file, "r") as f:
+        print(f.read())
+
+def remove_data(item):
+    with open(file, 'r') as f:
+        a = ast.literal_eval(json.loads(f.read()))
+    del a[item]
+    with open(file, 'w') as asd:
+        asd.write(json.dumps(a))
+
+
 
 create = 'c'
 read = 'r'
@@ -7,31 +32,31 @@ exit = 'e'
 
 contacts = {}
 
-def check_name(name):
+def check_name():
+    name = input('name: ')
     while len(name) < 3:
-        n = input('minimal 3 letters, please type name: ')
-        if len(n) >= 3:
-            return n
+        name = input('minimal 3 letters, please type name: ')
+    return name
 
-def check_phone(phone):
+def check_phone():
+    phone = input('phone: ')
     while len(phone) < 6:
-        p = input('minimal 6 numbers, please type phone: ')
-        if len(p) >= 6:
-            return phone
+        phone = input('minimal 6 numbers, please type phone: ')
+    return phone
 
 def action():
     while True:
         try:
             command = input('Enter your command: ')
             if command == create:
-                name = input('name: ')
-                n = check_name(name)
-                phone = input('phone: ')
-                p = check_phone(phone)
+                n = check_name()
+                p = check_phone()
                 contacts[n] = p
+                c = str(contacts)
+                write_to_file(c)
                 print('your contacts are: {}'.format(contacts))
             elif command == read:
-                print(contacts)
+                read_file()
             elif command == update:
                 change_name = input('print name: ')
                 change_phone = input('print phone: ')
@@ -39,9 +64,7 @@ def action():
                 print('your contacts are: {}'.format(contacts))
             elif command == delete:
                 motion = input('are you shure? type your "name": ')
-                if motion in contacts:
-                    del contacts[motion]
-                    print(contacts)
+                remove_data(motion)
             elif command == exit:
                 print('out from program')
                 break
