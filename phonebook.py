78b91a3ba9719e1df46c1245b079479596c8c3fe
idrummer83 <vodaadminwater@gmail.com>
2017-10-02 23:33:33 +0300

    csv, upd, del

--- phonebook.py
+++ phonebook.py
@@ -1,4 +1,4 @@
-import configparser, json, ast
+import configparser, json, csv
 config = configparser.ConfigParser()
 config.read('config.ini')
 
@@ -17,19 +17,42 @@ def read_file():
 
 def remove_data(item):
     with open(file, 'r') as f:
-        a = ast.literal_eval(json.loads(f.read()))
+        a = json.loads(f.read())
     del a[item]
     with open(file, 'w') as asd:
         asd.write(json.dumps(a))
+
+def update_data():
+    change_name = input('print name: ')
+    change_phone = input('print phone: ')
+    with open(file, 'r') as f:
+        a = json.loads(f.read())
+    a[change_name] = change_phone
+    with open(file, 'w') as asd:
+        asd.write(json.dumps(a))
+
+def save_csv():
+    with open(file, 'r') as f:
+        a = json.loads(f.read())
+    with open('phonebook.csv', 'w') as f:
+        writer = csv.writer(f)
+        for key, value in a.items():
+            writer.writerow([key, value])
+        # writer = csv.DictWriter(f, fieldnames=csv_columns)
+        # writer.writeheader()
+        # for data in a:
+        #     writer.writerow(data)
 
 
 create = 'c'
 read = 'r'
 update = 'u'
 delete = 'd'
+csv = 's'
 exit = 'e'
 
 contacts = {}
+csv_columns = ['Name','Phone']
 
 def check_name():
     name = input('name: ')
@@ -46,24 +69,22 @@ def check_phone():
 def action():
     while True:
         try:
-            command = input('Enter your command: ')
+            command = input('Enter your command: c(create), r(read), u(update), d(delete), s(save to csv), e(exit) : ')
             if command == create:
                 n = check_name()
                 p = check_phone()
                 contacts[n] = p
-                c = str(contacts)
-                write_to_file(c)
+                write_to_file(contacts)
                 print('your contacts are: {}'.format(contacts))
             elif command == read:
                 read_file()
             elif command == update:
-                change_name = input('print name: ')
-                change_phone = input('print phone: ')
-                contacts[change_name] = change_phone
-                print('your contacts are: {}'.format(contacts))
+                update_data()
             elif command == delete:
                 motion = input('are you shure? type your "name": ')
                 remove_data(motion)
+            elif command == csv:
+                save_csv()
             elif command == exit:
                 print('out from program')
                 break
