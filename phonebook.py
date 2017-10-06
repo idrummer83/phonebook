fffc4b5ad59a11953d470faf0027f8b871deeb25
idrummer83 <vodaadminwater@gmail.com>
2017-10-07 01:38:43 +0300

    oop

--- phonebook.py
+++ phonebook.py
@@ -1,10 +1,13 @@
 import configparser, json, csv
-config = configparser.ConfigParser()
-config.read('config.ini')
 
-name_file = config['FILE']['filename']
-format_file = config['FILE']['format']
-format_csv = config['FILE']['csv_format']
+def read_conf(a, b):
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+    return config[a][b]
+
+name_file = read_conf('FILE', 'filename')
+format_file = read_conf('FILE','format')
+format_csv = read_conf('FILE','csv_format')
 
 file = "{}.{}".format(name_file,format_file)
 csv_file = "{}.{}".format(name_file,format_csv)
@@ -16,81 +19,107 @@ delete = 'd'
 csv = 's'
 exit = 'e'
 
-contacts = {}
-csv_columns = ['Name','Phone']
-
-def write_to_file(a, b):
-    try:
+class Json:
+    def read(self):
         with open(file, 'r') as f:
-            aa = json.loads(f.read())
-            if aa:
-                aa[a] = b
-                with open(file, "w") as f:
-                    f.write(json.dumps(aa, file))
-    except:
-        contacts[a] = b
+            self.file_read = json.loads(f.read())
+            return self.file_read
+    def write(self, content):
         with open(file, "w") as f:
-            f.write(json.dumps(contacts, file))
+            self.file_write = f.write(json.dumps(content, file))
 
-def read_file():
-    with open(file, "r") as f:
-        print(f.read())
 
-def remove_data(item):
-    with open(file, 'r') as f:
-        a = json.loads(f.read())
-    del a[item]
-    with open(file, 'w') as asd:
-        asd.write(json.dumps(a))
+if read_conf('DEFAULT','json') == 'json':
+    db = Json() #class
+else:
+    db = csv() #class
 
-def update_data():
-    change_name = input('print name: ')
-    change_phone = input('print phone: ')
-    with open(file, 'r') as f:
-        a = json.loads(f.read())
-    a[change_name] = change_phone
-    with open(file, 'w') as asd:
-        asd.write(json.dumps(a))
 
-def check_name():
-    name = input('name: ')
-    while len(name) < 3:
-        name = input('minimal 3 letters, please type name: ')
-    return name
+class Crud:
+    def __init__(self, db):
+        try:
+            self.pb = db.read()
+        except:
+            self.pb = {}
 
-def check_phone():
-    phone = input('phone: ')
-    while len(phone) < 6:
-        phone = input('minimal 6 numbers, please type phone: ')
-    return phone
+    def write_to_file(self, name, phone):
+        self.pb[name] = phone
+        db.write(self.pb)
+
+    def read_file(self):
+        print(self.pb)
+
+    def remove_data(self, item):
+        items = self.pb
+        del items[item]
+        db.write(items)
+
+    def update_data(self):
+        change_name = input('print name: ')
+        change_phone = input('print phone: ')
+        items = self.pb
+        items[change_name] = change_phone
+        db.write(items)
+
+
+crud = Crud(db)
 
 def save_csv():
     try:
         with open(file, 'r') as f:
             aa = json.loads(f.read())
-            if aa:
+            if len(aa) > 0:
                 with open('mycsvfile.csv', 'w') as f:
                     w = csv.Writer(f)
-                    w.writerows(aa.items())
+                    for r in aa:
+                        w.writerows(aa.items(r))
     except:
         pass
 
+    # def check_name():
+    #     name = input('name: ')
+    #     while len(name) < 3:
+    #         name = input('minimal 3 letters, please type name: ')
+    #     return name
+    #
+    # def check_phone():
+    #     phone = input('phone: ')
+    #     while len(phone) < 6:
+    #         phone = input('minimal 6 numbers, please type phone: ')
+    #     return phone
 
+
+class Validate:
+    def check_name(self):
+        self.name = input('name: ')
+        while len(self.name) < 3:
+            self.name = input('minimal 3 letters, please type name: ')
+        return self.name
+
+    def check_phone(self):
+        self.phone = input('phone: ')
+        while len(self.phone) < 6:
+            self.phone = input('minimal 6 numbers, please type phone: ')
+        return self.phone
+
+valid = Validate()
+
+# controller
 def action():
     while True:
         try:
             command = input('Enter your command: c(create), r(read), u(update), d(delete), s(save to csv), e(exit) : ')
             if command == create:
-                n = check_name()
-                p = check_phone()
-                write_to_file(n, p)
+                n = valid.check_name()
+                p = valid.check_phone()
+                crud.write_to_file(n,p)
             elif command == read:
-                read_file()
+                crud.read_file()
             elif command == update:
-                update_data()
+                crud.update_data()
             elif command == delete:
-                motion = input('are you shure? type your "name": ')
-                remove_data(motion)
+                item = input('are you shure? type your "name": ')
+                crud.remove_data(item)
             elif command == csv:
                 save_csv()
             elif command == exit:
